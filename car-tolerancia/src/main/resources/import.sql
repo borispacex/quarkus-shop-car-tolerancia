184583747c12b52c03895d53e985760b503f9f33
Boris Vargas <borisvargaspaucara@gmail.com>
2023-08-22 21:18:31 -0400

    subiendo clase 20 concluida

--- car-tolerancia/src/main/resources/import.sql
+++ car-tolerancia/src/main/resources/import.sql
@@ -1,6 +1,13 @@
 -- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+
+insert into shop(id, state, address) values (1, 'Ohio', 'Ts. Philips #740');
+insert into shop(id, state, address) values (2, 'La Florida', 'Boulevard Saint Vicent #613, Miami');
+
+insert into car(id, model, bodyType, brand, year, price, seats, doors, urlImage, shop_id)
+values (1, 'BMW 525', 'sedan', 'BMW', '2007', 8500.0, 5, 4, 'image0', 1);
+
+insert into car(id, model, bodyType, brand, year, price, seats, doors, urlImage, shop_id)
+values (2, 'BMW 316', 'sedan', 'BMW', '2008', 9500.0, 5, 4, 'image0', 1);
+
+insert into car(id, model, bodyType, brand, year, price, seats, doors, urlImage, shop_id)
+values (3, 'BMW 318', 'sedan', 'BMW', '2009', 10500.0, 5, 4, 'image0', 2);
--- shop-tolerancia/src/main/java/org/redeasy/shop/resource/ShopResource.java
+++ shop-tolerancia/src/main/java/org/redeasy/shop/resource/ShopResource.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import org.eclipse.microprofile.faulttolerance.*;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.redeasy.shop.entity.Car;
 import org.redeasy.shop.entity.Shop;
@@ -14,7 +15,9 @@ import org.redeasy.shop.repository.CarRepository;
 import org.redeasy.shop.repository.ShopRepository;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Path("/shops")
 public class ShopResource {
@@ -25,16 +28,28 @@ public class ShopResource {
     @Inject
     ShopRepository shopRepository;
 
+    // Metodo con Tolerancia a fallos, Tiempo de espera
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Timeout(5000)
     public Response getAll() {
+        pausa(3000L); // si es mayor al timeout falla
         List<Shop> shops = shopRepository.listAll();
         return Response.ok(shops).build();
     }
+    private void pausa(Long milisegundos) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(milisegundos);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
+    // Metodo get Cars con Tolerancia a fallos
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}")
+    @Retry(maxRetries = 4)
     public Response getById(@PathParam("id") Long id) {
         Shop shop = shopRepository.findById(id);
         if (shopRepository.isPersistent(shop)) {
@@ -74,12 +89,24 @@ public class ShopResource {
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
-    // Metodo obtenido desde Car
+    // Metodo obtenido desde Car, usando Tolerancia a fallos
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}/cars")
+    @Fallback(fallbackMethod = "fallbackGetCars")
+    @CircuitBreaker(requestVolumeThreshold = 4, failureRatio = 0.5, delay = 4000, successThreshold = 2)
+    @Bulkhead(value = 2, waitingTaskQueue = 10)
     public Response getCars(@PathParam("id") Long id) {
         List<Car> cars = carProxy.getCarsByShopId(id);
+        System.out.println("[INFO] Consultando el {id: " + id + "} desde el metodo getCars");
         return Response.ok(cars).build();
     }
+
+    // Metodo si ocurre un fallo, nos vamos a ejecuta este metodo
+    private Response fallbackGetCars(Long id) {
+        List<Car> cars = new ArrayList<Car>();
+        System.out.println("[INFO] Consultando el {id: " + id + "} desde el fallback method fallbackGetCars");
+        return Response.ok(cars).build();
+    }
+
 }
